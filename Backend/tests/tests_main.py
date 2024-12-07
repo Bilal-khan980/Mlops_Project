440b0a0d54d9f6a15e864e4a2cf0423e81993155
Bilal-khan980 <bilal4412192@gmail.com>
2024-12-07 22:38:32 +0500

    Hello

--- Backend/tests/tests_main.py
+++ Backend/tests/tests_main.py
@@ -7,7 +7,7 @@ from fastapi import HTTPException
 from passlib.context import CryptContext  # Import passlib for password hashing
 
 # Add the project root directory to sys.path so Python can find mlops_project
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(_file_), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import signup, login, predict  # Import from app.py
 from pydantic import BaseModel
